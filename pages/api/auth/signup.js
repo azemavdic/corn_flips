ae4a1fe05079f865719196e894a7598ab554e48a
Asko2212 <70911882+Asko2212@users.noreply.github.com>
2021-10-05 01:22:39 +0200

    register page

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,37 @@
+import User from "../../../models/User";
+import { hashPassword } from "../../../lib/auth";
+
+export default async function (req, res) {
+  if (req.method === "POST") {
+    const { username, email, password } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !password ||
+      password.trim().length < 7
+    ) {
+      res.status(422).json({
+        message: "Netačan unos podataka - šifra je minimalno 7 karaktera",
+      });
+      return;
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res
+        .status(422)
+        .json({ message: "Korisnik postoji sa tom email adresom" });
+      return;
+    }
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    res.status(201).json({ message: "Kreiran korisnik" });
+  }
+}
---
+++ pages/auth/signup.js
@@ -0,0 +1,58 @@
+import { useRef } from "react";
+import styles from "../../css/Form.module.css";
+
+const SignUpPage = () => {
+  const usernameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const enteredUsername = usernameRef.current.value;
+    const enteredEmail = emailRef.current.value;
+    const enteredPassword = passwordRef.current.value;
+    const user = {
+      username: enteredUsername,
+      email: enteredEmail,
+      password: enteredPassword,
+    };
+
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+
+    const data = await res.json();
+
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h1 className={styles.center}>Registracija</h1>
+      <div className={styles.form}>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='username'>Korisničko ime</label>
+            <input type='text' id='username' ref={usernameRef} />
+          </div>
+          <div>
+            <label htmlFor='email'>Email</label>
+            <input type='email' id='email' ref={emailRef} />
+          </div>
+          <div>
+            <label htmlFor='password'>Šifra</label>
+            <input type='password' id='password' ref={passwordRef} />
+          </div>
+          <input type='submit' value='Registracija' />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
