066e11347469cfebfbb90abc244dfe19848b1773
Asko2212 <70911882+Asko2212@users.noreply.github.com>
2021-10-09 11:09:41 +0200

    mijenjanje ovlastenja

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,26 +1,36 @@
-import User from '../../../models/User';
+import User from "../../../models/User";
 
 export default async function (req, res) {
-    const id = req.query.id;
-    if (req.method === 'DELETE') {
-        try {
-            const user = await User.deleteOne({ _id: id });
+  const id = req.query.id;
+  if (req.method === "DELETE") {
+    try {
+      const user = await User.deleteOne({ _id: id });
 
-            if (!user) {
-                res.status(404).json({ message: 'Korisnik nije pronađen' });
-                return;
-            }
-            res.status(200).json({ message: 'Obrisan korisnik' });
-        } catch (error) {
-            res.status(400).json({ message: error });
-        }
-    } else if (req.method === 'GET') {
-        const user = await User.findById({ _id: id });
-        if (!user) {
-            res.status(404).json({ message: 'Korisnik nije pronađen' });
-            return;
-        }
-        res.status(200).json({ user });
+      if (!user) {
+        res.status(404).json({ message: "Korisnik nije pronađen" });
         return;
+      }
+      res.status(200).json({ message: "Obrisan korisnik" });
+    } catch (error) {
+      res.status(400).json({ message: error });
     }
+  } else if (req.method === "GET") {
+    const user = await User.findById({ _id: id });
+    if (!user) {
+      res.status(404).json({ message: "Korisnik nije pronađen" });
+      return;
+    }
+    res.status(200).json({ user });
+    return;
+  } else if (req.method === "PUT") {
+    const user = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      res.status(400).json({ message: "Korisnik nije pronađen" });
+      return;
+    }
+    res.status(200).json({ message: "Uspješna promjena" });
+  }
 }
--- pages/users/edit/[id].js
+++ pages/users/edit/[id].js
@@ -1,49 +1,108 @@
-import { getSession } from 'next-auth/client';
-import dbConnect from '../../../lib/dbConnect';
-import User from '../../../models/User';
-import styles from '../../../css/User.module.css';
+import { getSession } from "next-auth/client";
+import dbConnect from "../../../lib/dbConnect";
+import User from "../../../models/User";
+import styles from "../../../css/User.module.css";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { useRouter } from "next/router";
 
 const EditUserPage = ({ user }) => {
-    let ovlastenja;
+  const { isProizvodnja, isKomercijala, isAdmin } = user;
+  const [proizvodnja, setProizvodnja] = useState(isProizvodnja);
+  const [komercijala, setKomercijala] = useState(isKomercijala);
+  const [admin, setAdmin] = useState(isAdmin);
 
-    if (user.isAdmin) {
-        ovlastenja = 'Admin';
-    } else if (user.isProizvodnja) {
-        ovlastenja = 'Proizvodnja';
-    } else {
-        ovlastenja = 'Komercijala';
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const ovlastenja = {
+      isProizvodnja: proizvodnja,
+      isKomercijala: komercijala,
+      isAdmin: admin,
+    };
+    const res = await fetch(`/api/users/${user._id}`, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(ovlastenja),
+    });
+
+    const data = await res.json();
+    if (!res.ok) {
+      toast.error(data.message);
+      return;
     }
+    toast.success(data.message);
+    router.push("/users");
+  };
 
-    return (
-        <div className={styles.container}>
-            <h1>{user.name}</h1>
-            <hr style={{ marginTop: '10px' }} />
-            <div className={styles.body}>
-                <p>Email: {user.email}</p>
-                <p>Ovlaštenja: {ovlastenja}</p>
+  return (
+    <div className={styles.container}>
+      <h1>{user.name}</h1>
+      <hr style={{ marginTop: "10px" }} />
+      <div className={styles.body}>
+        <p>Ovlaštenja</p>
+        <div className={styles.box}>
+          <form onSubmit={handleSubmit}>
+            <div className={styles.flex}>
+              <label>Proizvodnja</label>
+              <input
+                type='checkbox'
+                name='ovlastenja'
+                id='prizvodnja'
+                checked={proizvodnja}
+                onChange={(e) => setProizvodnja(!proizvodnja)}
+              />
             </div>
+            <div className={styles.flex}>
+              <label htmlFor='komercijala'>Komercijala</label>
+              <input
+                type='checkbox'
+                name='ovlastenja'
+                id='komercijala'
+                checked={komercijala}
+                onChange={(e) => setKomercijala(!komercijala)}
+              />
+            </div>
+            <div className={styles.flex}>
+              <label htmlFor='admin'>Admin</label>
+              <input
+                type='checkbox'
+                name='ovlastenja'
+                id='admin'
+                checked={admin}
+                onChange={(e) => setAdmin(!admin)}
+              />
+            </div>
+            <input type='submit' value='Potvrdi' />
+          </form>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export async function getServerSideProps(ctx) {
-    const session = await getSession(ctx);
-    if (!session) {
-        return {
-            redirect: {
-                destination: '/auth/login',
-                permanent: false,
-            },
-        };
-    }
-    await dbConnect();
+  const session = await getSession(ctx);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/login",
+        permanent: false,
+      },
+    };
+  }
+  await dbConnect();
 
-    const id = ctx.query.id;
-    const data = await User.findById(id).lean();
+  const id = ctx.query.id;
+  const data = await User.findById(id).lean();
 
-    const user = JSON.parse(JSON.stringify(data));
+  const user = JSON.parse(JSON.stringify(data));
 
-    return { props: { session, user } };
+  return { props: { session, user } };
 }
 
 export default EditUserPage;
