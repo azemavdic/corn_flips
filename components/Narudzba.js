f3868c529a1a6c8545993bfa132d273024ecddff
unknown <asko2212@hotmail.com>
2021-10-13 08:33:58 +0200

    dodane grupe i proizvodi u padajuću listu

--- components/Narudzba.js
+++ components/Narudzba.js
@@ -1,78 +1,71 @@
-import styles from "../css/Form.module.css";
-import Select from "react-select";
-
-const options = [
-  { value: "flips25", label: "Gold flips 25g" },
-  { value: "flips50", label: "Gold flips 50g" },
-  { value: "flips140", label: "Gold flips 140g" },
-  { value: "cips30", label: "Gold chips 30g" },
-  { value: "cips85", label: "Gold chips 85g slani" },
-];
+import styles from '../css/Form.module.css';
+import Select from 'react-select';
+import { options } from '../data/options';
 
 const customStyles = {
-  option: (provided, state) => ({
-    ...provided,
-    fontSize: "14px",
-  }),
-  singleValue: (provided, state) => ({
-    ...provided,
-    fontSize: "14px",
-  }),
-  input: (provided, state) => ({
-    ...provided,
-    width: "150px",
-  }),
+    option: (provided, state) => ({
+        ...provided,
+        fontSize: '14px',
+    }),
+    singleValue: (provided, state) => ({
+        ...provided,
+        fontSize: '14px',
+    }),
+    input: (provided, state) => ({
+        ...provided,
+        width: '150px',
+    }),
 };
 
 const Narudzba = ({
-  el,
-  i,
-  onInputChange,
-  onRemove,
-  onAdd,
-  inputList,
-  setSelect,
-  // select,
+    el,
+    i,
+    onInputChange,
+    onRemove,
+    onAdd,
+    inputList,
+    setSelect,
+    // select,
 }) => {
-  return (
-    <div className={styles.modal}>
-      {/* <input
+    return (
+        <div className={styles.modal}>
+            {/* <input
         type='text'
         name='naziv'
         value={el.naziv}
         placeholder='naziv'
         onChange={(e) => onInputChange(e, i)}
       /> */}
-      <Select
-        placeholder='Odaberi proizvod'
-        onChange={setSelect}
-        options={options}
-        name='naziv'
-        styles={customStyles}
-        // className={styles.select}
-      />
-      <input
-        className={styles.ml10}
-        type='text'
-        name='kolicina'
-        value={el.kolicina}
-        placeholder='količina u kutijama'
-        onChange={(e) => onInputChange(e, i)}
-      />
-      <div className={styles.btnbox}>
-        {inputList.length !== 1 && (
-          <button className={styles.red} onClick={() => onRemove(i)}>
-            Izbriši
-          </button>
-        )}
-        {inputList.length - 1 === i && (
-          <button className={styles.green} onClick={onAdd}>
-            Novi
-          </button>
-        )}
-      </div>
-    </div>
-  );
+            <Select
+                placeholder='Odaberi proizvod'
+                onChange={setSelect}
+                options={options}
+                name='naziv'
+                styles={customStyles}
+                // className={styles.select}
+            />
+            <input
+                className={styles.ml10}
+                type='text'
+                name='kolicina'
+                value={el.kolicina}
+                placeholder='količina u kutijama'
+                onChange={(e) => onInputChange(e, i)}
+            />
+            <div className={styles.btnbox}>
+                {inputList.length !== 1 && (
+                    <button className={styles.red} onClick={() => onRemove(i)}>
+                        Izbriši
+                    </button>
+                )}
+                {inputList.length - 1 === i && (
+                    <button className={styles.green} onClick={onAdd}>
+                        Novi
+                    </button>
+                )}
+            </div>
+        </div>
+    );
 };
 
 export default Narudzba;
---
+++ data/options.js
@@ -0,0 +1,84 @@
+const flips = [
+    { value: 'flips25', label: 'Gold flips 25g' },
+    { value: 'flips50', label: 'Gold flips 50g' },
+    { value: 'flips100', label: 'Gold flips 100g' },
+    { value: 'flips140', label: 'Gold flips 140g' },
+    { value: 'flips140/14', label: 'Gold flips 140g 14/1' },
+    { value: 'pikant50', label: 'Gold piquant 50g' },
+    { value: 'crispy75', label: 'Gold crispy 75g' },
+];
+
+const cips = [
+    { value: 'cips100sd', label: 'Čips SD 100g slani' },
+    { value: 'cips50sd', label: 'Čips SD 50g slani' },
+    { value: 'cips30sl', label: 'Gold chips 30g slani' },
+    { value: 'cips30p', label: 'Gold chips 30g paprika' },
+    { value: 'cips30sm', label: 'Gold chips 30g s.m.' },
+    { value: 'cips85sl', label: 'Gold chips 85g slani' },
+    { value: 'cips85p', label: 'Gold chips 85g reb.pap.' },
+    { value: 'cips180sl', label: 'Gold chips 180g slani' },
+    { value: 'cips180p', label: 'Gold chips 180g paprika' },
+    { value: 'cips180sm', label: 'Gold chips 180g reb. s.m.' },
+    { value: 'cips170č', label: 'Gold chips 170g chili' },
+    { value: 'cips140sl', label: 'Super čips 140g slani' },
+    { value: 'cips30sl', label: 'Super čips 30g slani' },
+    { value: 'pelete35pil', label: 'Gold pellete 35g pil.' },
+    { value: 'bruskete50', label: 'Gold bruschette 50g' },
+];
+
+const popcorn = [
+    { value: 'popcorn50gsl', label: 'Gold popcorn 50g slani' },
+    { value: 'popcorn100gsl', label: 'Gold popcorn 100g slani' },
+    { value: 'popcorn50gk', label: 'Gold popcorn 50g kik.' },
+];
+
+const slatki = [
+    { value: 'cokobadem150', label: 'Čoko badem u cimetu 150g' },
+    { value: 'cokobadem100', label: 'Badem u čokol. i cim. 100g' },
+    { value: 'cokobrusn100', label: 'Brusnica u čokol. i cim. 100g' },
+    { value: 'cokogroz100', label: 'Grožđice u čokol. i cim. 100g' },
+    { value: 'cokokik100', label: 'Kikiriki u čokoladi 100g' },
+    { value: 'cokoflips40', label: 'Čoko flips 40g' },
+    { value: 'pillows40', label: 'Gold pillows 40g' },
+    { value: 'pillows80', label: 'Gold pillows 80g' },
+];
+
+const kikiriki = [
+    { value: 'kikiriki50sl', label: 'Kikiriki 50g CF slani' },
+    { value: 'kikiriki100sl', label: 'Kikiriki 100g CF slani' },
+    { value: 'kikiriki200sl', label: 'Kikiriki 200g CF slani' },
+    { value: 'kikiriki80p', label: 'Kikiriki 80g CF paprika' },
+    { value: 'kikiriki200p', label: 'Kikiriki 200g CF paprika' },
+];
+
+const krem = [
+    { value: 'krem450', label: 'Gold nutt 450g' },
+    { value: 'krem900', label: 'Gold nutt 900g' },
+];
+
+export const options = [
+    {
+        label: 'Flips proizvodi',
+        options: flips,
+    },
+    {
+        label: 'Čips proizvodi',
+        options: cips,
+    },
+    {
+        label: 'Popcorn',
+        options: popcorn,
+    },
+    {
+        label: 'Slatki proizvodi',
+        options: slatki,
+    },
+    {
+        label: 'Kikiriki proizvodi',
+        options: kikiriki,
+    },
+    {
+        label: 'Krem proizvodi',
+        options: krem,
+    },
+];
