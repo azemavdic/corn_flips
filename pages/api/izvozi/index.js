576eac893fe7f381e41da4f37caa1a049f6f9c78
unknown <asko2212@hotmail.com>
2022-02-09 12:55:00 +0100

    odabir godine zavrsen

--- pages/api/izvozi/index.js
+++ pages/api/izvozi/index.js
@@ -1,41 +1,42 @@
-import dbConnect from "../../../lib/dbConnect";
-import Izvoz from "../../../models/Izvoz";
+import dbConnect from '../../../lib/dbConnect';
+import Izvoz from '../../../models/Izvoz';
 
 export default async function handler(req, res) {
-  const { method } = req;
+    const { method } = req;
 
-  await dbConnect();
+    await dbConnect();
 
-  switch (method) {
-    case "GET":
-      try {
-        const izvozi = await Izvoz.find(
-          {}
-        ); /* find all the data in our database */
-        res.status(200).json({ success: true, data: izvozi });
-      } catch (error) {
-        res.status(400).json({ success: false });
-      }
-      break;
-    case "POST":
-      try {
-        const { naziv, narudzba, isporuka } = req.body;
-        if (!naziv || !narudzba || !isporuka) {
-          res.status(422).json({
-            message: "Molimo popunite sva polja",
-          });
-          return;
-        }
-        const izvoz = await Izvoz.create(
-          req.body
-        ); /* create a new model in the database */
-        res.status(201).json({ success: true, data: izvoz });
-      } catch (error) {
-        res.status(400).json({ success: false });
-      }
-      break;
-    default:
-      res.status(400).json({ success: false });
-      break;
-  }
+    switch (method) {
+        case 'GET':
+            try {
+                const izvozi = await Izvoz.find({}).sort({
+                    zavrsen: 1,
+                    isporuka: 1,
+                }); /* find all the data in our database */
+                res.status(200).json({ success: true, data: izvozi });
+            } catch (error) {
+                res.status(400).json({ success: false });
+            }
+            break;
+        case 'POST':
+            try {
+                const { naziv, narudzba, isporuka } = req.body;
+                if (!naziv || !narudzba || !isporuka) {
+                    res.status(422).json({
+                        message: 'Molimo popunite sva polja',
+                    });
+                    return;
+                }
+                const izvoz = await Izvoz.create(
+                    req.body
+                ); /* create a new model in the database */
+                res.status(201).json({ success: true, data: izvoz });
+            } catch (error) {
+                res.status(400).json({ success: false });
+            }
+            break;
+        default:
+            res.status(400).json({ success: false });
+            break;
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,44 @@ import styles from '../css/Home.module.css';
 import Izvoz from '../components/Izvoz';
 import Link from 'next/link';
 import { getSession } from 'next-auth/client';
+import { useGetIzvoziQuery } from '../redux/api/api';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-const Index = ({ izvozi, user }) => {
+const Index = ({ user }) => {
+    const [odabranaGodina, setOdabranaGodina] = useState(
+        new Date().getFullYear()
+    );
+
+    const { izvoziIsporuka } = useGetIzvoziQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            izvoziIsporuka: data.filter((izvoz) => {
+                const god = new Date(izvoz.isporuka).getFullYear();
+                return god === Number(odabranaGodina);
+            }),
+        }),
+    });
+
     const proizvodnja = user.isProizvodnja;
     let index = 1;
     if (!user.isProizvodnja && !user.isKomercijala && !user.isAdmin) {
         return <p>Nemate pristup platformi</p>;
     }
+
+    const handleChange = (e) => {
+        setOdabranaGodina(e.target.value);
+    };
     return (
         <>
             <div className={styles.header}>
                 <h1>Raspored izvoza</h1>
+                <div className={styles.select}>
+                    <select value={odabranaGodina} onChange={handleChange}>
+                        <option value='2021'>2021</option>
+                        <option value='2022'>2022</option>
+                        <option value='2023'>2023</option>
+                    </select>
+                </div>
                 {!proizvodnja && (
                     <Link href='/dodaj'>
                         <a>Novi izvoz</a>
@@ -37,8 +64,8 @@ const Index = ({ izvozi, user }) => {
                         </tr>
                     </thead>
                     <tbody>
-                        {izvozi &&
-                            izvozi.map((izvoz) => (
+                        {izvoziIsporuka &&
+                            izvoziIsporuka.map((izvoz) => (
                                 <Izvoz
                                     key={izvoz._id}
                                     izvoz={izvoz}
@@ -74,9 +101,9 @@ export async function getServerSideProps(ctx) {
     const userData = await UserModel.findOne({ name: session.user.name });
     const user = JSON.parse(JSON.stringify(userData));
 
-    const izvozi = JSON.parse(JSON.stringify(result));
+    // const izvozi = JSON.parse(JSON.stringify(result));
 
-    return { props: { izvozi, session, user } };
+    return { props: { session, user } };
 }
 
 export default Index;
